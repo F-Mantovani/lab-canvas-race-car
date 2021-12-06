175d5d3d1994ab366d60ca7046ef9d38a27a11d3
Felipe Leite Mantovani <mantovani.devilz@gmail.com>
2021-12-06 09:14:24 -0300

    consegui colocar o background e o context clicando no botão start game

--- js/index.js
+++ js/index.js
@@ -3,5 +3,23 @@ window.onload = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    
+
+    const board = document.getElementById('game-board');
+    board.style.backgroundImage = "url('./images/road.png')";
+    board.style.backgroundPosition = "center";
+    board.style.backgroundRepeat = "no-repeat";
+    board.style.backgroundSize = "cover";
+  }
 };
+
+
+class Game {
+  constructor(){
+    this.frames = 0;
+    this.points = 0;
+  }
+}
